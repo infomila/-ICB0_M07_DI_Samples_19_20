7c9aad14c41685e7ee6739d98f9192ead139fce7
Bernat <bernat.orellana@gmail.com>
2019-10-01 20:30:13 +0200

    Exemple de gestió de Vehicles

---
+++ 3_gestio_vehicles/2_classes_i_collecions_uwp/model/Marca.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_classes_i_collecions_uwp.model
+{
+    class Marca
+    {
+        //--- Singleton -------------------------
+        private static List<Marca> marques;
+
+        public static List<Marca> GetMarques()
+        {
+            if (marques == null) // inicialitzem la llista si no té valor
+            {
+                marques = new List<Marca>();
+                //-------------------------------
+                Marca seat = new Marca("Seat");
+                seat.models.Add("Leon");
+                seat.models.Add("Tarraco");
+                seat.models.Add("Exeo");
+                marques.Add(seat);
+                //-------------------------------
+                Marca volkswagen = new Marca("Volkswagen");
+                volkswagen.models.Add("Golf");
+                volkswagen.models.Add("Touran");
+                volkswagen.models.Add("California");
+                marques.Add(volkswagen);
+            }
+            return marques;
+        }
+
+        //---------------------------------------------
+        private string nom;
+        private List<string> models;
+        //---------------------------------------------
+        public Marca(string pNom)
+        {
+            nom = pNom;
+            models = new List<string>();
+        }
+
+        //---------------------------------------------
+        public string Nom { get => nom; }
+        public List<string> Models {
+            get => models;
+            }
+        //---------------------------------------------
+
+    }
+}
---
+++ 3_gestio_vehicles/2_classes_i_collecions_uwp/model/Vehicle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_classes_i_collecions_uwp.model
+{
+    class Vehicle
+    {
+        private static List<Vehicle> vehicles;
+
+        public static List<Vehicle> GetVehicles()
+        {
+            if (vehicles == null)
+            {
+                vehicles = new List<Vehicle>();
+                Vehicle v1 = new Vehicle(1, "1234JKJ", "Seat", "Leon", EnumTipus.COTXE);
+                Vehicle v2 = new Vehicle(2, "9999GGG", "Seat", "Exeo", EnumTipus.COTXE);
+                Vehicle v3 = new Vehicle(3, "3333JJJ", "Volkswagen", "Golf", EnumTipus.COTXE);
+                vehicles.Add(v1);
+                vehicles.Add(v2);
+                vehicles.Add(v3);
+                //vehicles.AddRange(new List<Vehicle>{ v1,v2,v3});
+            }
+            return vehicles;
+        }
+
+
+
+
+        //-----------------------------------------------
+
+
+        private int codi;
+        private string matricula;
+        private string marca;
+        private string model;
+        private EnumTipus tipus;
+
+        public Vehicle(int codi, string matricula, string marca, string model, EnumTipus tipus)
+        {
+            this.Codi = codi;
+            this.Matricula = matricula;
+            this.Marca = marca;
+            this.Model = model;
+            this.Tipus = tipus;
+        }
+
+        public int Codi { get => codi; set => codi = value; }
+        public string Matricula {
+                            get => matricula;
+                            set  {
+                if (value == null || value.Length != 7)
+                    throw new Exception("MATRICULA NO VÀLIDA");
+                                matricula = value;
+                            }
+        }
+        public string Marca { get => marca; set => marca = value; }
+        public string Model { get => model; set => model = value; }
+        public EnumTipus Tipus { get => tipus; set => tipus = value; }
+
+
+        
+        public override bool Equals(object obj)
+        {
+            var vehicle = obj as Vehicle;
+            return vehicle != null &&
+                   matricula == vehicle.matricula;
+        }
+
+        
+        public override String ToString()
+        {
+            return Codi + " " + Matricula + " " + Marca;
+        }
+
+
+        /*
+        public Vehicle(int pcodi, string pmatricula, string pmarca, string pmodel , string ptipus)
+        {
+            this.codi = pcodi;
+            this.matricula = pmatricula;
+        }*/
+    }
+}
