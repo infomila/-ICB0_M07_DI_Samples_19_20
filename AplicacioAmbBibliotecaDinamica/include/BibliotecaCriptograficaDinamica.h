38812ebe5378a7b968af3b3d6c19948b665329ba
Bernat <bernat.orellana@gmail.com>
2019-10-02 06:23:51 +0200

    Exemple alta-baixa-modificació

--- AplicacioAmbBibliotecaDinamica/include/BibliotecaCriptograficaDinamica.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined DLL_EXPORT
-#define DECLDIR __declspec(dllexport)
-#else
-#define DECLDIR __declspec(dllimport)
-#endif
-
-extern "C" DECLDIR int suma(int a, int b);
--- BibliotecaCriptograficaEstatica/FuncionsCriptografiques.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-#include "FuncionsCriptografiques.h"
-
-int hash(int a) {
-	return a + 3;
-}
-
