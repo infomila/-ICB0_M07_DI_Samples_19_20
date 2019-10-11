8f5f81bd7532c3003bf107d929bd63481d417897
Bernat <bernat.orellana@gmail.com>
2019-10-11 15:13:54 +0200

    Preparació Binding

---
+++ 4_gestio_vehicles_databinding/2_classes_i_collecions_uwp/MainPage.xaml.cs
@@ -0,0 +1,365 @@
+﻿using _2_classes_i_collecions_uwp.model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _2_classes_i_collecions_uwp
+{
+   
+
+    enum ModeEdicio
+    {
+        MODE_EDIT,
+        MODE_NEW
+    }
+
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private int indexVehicleActual = 0;
+        private List<Vehicle> vehicles;
+
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //----------------------------------------------------
+            // precarreguem la llista de vehicles en un atribut de la classe
+            // per disposa-la en qualsevol moment
+            vehicles = Vehicle.GetVehicles();
+
+
+            //----------------------------------------------------
+            // Carreguem el combobox de marques
+            cboMarca.ItemsSource = Marca.GetMarques();
+            cboMarca.DisplayMemberPath = "Nom";
+
+            //----------------------------------------------------
+            // Mostrem el vehicle actual ( inicialment serà zero )
+            MostrarVehicle(indexVehicleActual);
+        }
+        private void MostrarVehicle( int index )
+        {
+     
+            //List<Vehicle> vehicles = Vehicle.GetVehicles();
+            if (vehicles.Count> index && index>=0)
+            {
+                MostrarVehicle(vehicles[index]);
+            }
+   
+      
+        }
+
+
+        private void DesactivaEvents()
+        {
+            txbMatricula.TextChanged -= txbMatricula_TextChanged;
+            cboMarca.SelectionChanged -= cboMarca_SelectionChanged;
+            cboModel.SelectionChanged -= cboModel_SelectionChanged;
+            rdoCotxe.Checked -= rdoCotxe_Checked;
+            rdoMoto.Unchecked -= rdoCotxe_Checked;
+            rdoMoto.Checked -= rdoCotxe_Checked;
+        }
+        private void ActivaEvents()
+        {
+            txbMatricula.TextChanged  += txbMatricula_TextChanged;
+            cboMarca.SelectionChanged += cboMarca_SelectionChanged;
+            cboModel.SelectionChanged += cboModel_SelectionChanged;
+            rdoCotxe.Checked  += rdoCotxe_Checked;
+            rdoMoto.Unchecked += rdoCotxe_Checked;
+            rdoMoto.Checked   += rdoCotxe_Checked;
+        }
+
+
+        private void MostrarVehicle(Vehicle v)
+        {
+            txbCodi.Text = v.Codi.ToString();
+            txbMatricula.Text = v.Matricula;
+            if (v.Tipus == EnumTipus.COTXE)
+            {
+                rdoCotxe.IsChecked = true;
+            }
+            else
+            {
+                rdoMoto.IsChecked = true;
+            }
+            //----------------------------------------
+            string marca = v.Marca;
+            cboMarca.SelectedValuePath = "Nom";
+            cboMarca.SelectedValue = marca;
+            cboMarca_SelectionChanged(null, null);
+            cboModel.SelectedValue = v.Model;
+        }
+
+        private void cboMarca_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //int indexMarcaSeleccionada = cboMarca.SelectedIndex;
+            //List<Marca> marques = Marca.GetMarques();
+            //Marca seleccionada = marques[indexMarcaSeleccionada];
+            Marca seleccionada = (Marca)cboMarca.SelectedItem;
+            if (seleccionada != null)
+            {
+                cboModel.ItemsSource = seleccionada.Models;
+            } else
+            {
+                cboModel.ItemsSource = null;
+            }
+
+
+        }
+
+        private async void Button_Click_Endavant(object sender, RoutedEventArgs e)
+        {
+             
+            if (await verificaCanvisPendentsAsync())
+            {
+                this.indexVehicleActual = (this.indexVehicleActual + 1) % vehicles.Count;
+                MostrarVehicle(indexVehicleActual);
+            }
+        }
+
+
+        private async System.Threading.Tasks.Task<bool> verificaCanvisPendentsAsync()
+        {
+
+            try
+            {
+                Vehicle modificat = new Vehicle(
+                    Int32.Parse(txbCodi.Text),
+                    txbMatricula.Text,
+                    (string)cboMarca.SelectedValue,
+                    (string)cboModel.SelectedValue,
+                    (rdoCotxe.IsChecked == true) ? EnumTipus.COTXE : EnumTipus.MOTO
+                );
+                Vehicle original = vehicles[indexVehicleActual];
+
+                if (original.Equals(modificat)) return true;
+            } catch(Exception ex)
+            {
+
+            }
+
+            ContentDialog confirmaFileDialog = new ContentDialog
+            {
+                Title = "Atenció !",
+                Content = "Hi ha canvis pendents, vol descartar canvis?",
+                PrimaryButtonText = "Descartar",
+                CloseButtonText = "Cancel·lar"
+            };
+
+            ContentDialogResult result = await confirmaFileDialog.ShowAsync();
+
+            // Delete the file if the user clicked the primary button.
+            /// Otherwise, do nothing.
+            if (result == ContentDialogResult.Primary)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+        private async void Button_Click_Enrere
+            (object sender, RoutedEventArgs e)
+        {
+            if (await verificaCanvisPendentsAsync())
+            {
+
+                this.indexVehicleActual--;
+                if (this.indexVehicleActual < 0)
+                {
+                    this.indexVehicleActual = vehicles.Count - 1;
+                }
+                MostrarVehicle(indexVehicleActual);
+            }
+        }
+
+        private void Button_Click_Save(object sender, RoutedEventArgs e)
+        {
+            if (ValidaCamps())
+            {
+                if(modeActual == ModeEdicio.MODE_NEW)
+                {
+                    Vehicle nou = new Vehicle(
+                            Int32.Parse( txbCodi.Text ),
+                            txbMatricula.Text,
+                            (string)cboMarca.SelectedValue,
+                            (string)cboModel.SelectedValue,
+                            (rdoCotxe.IsChecked == true) ? EnumTipus.COTXE : EnumTipus.MOTO
+                        );
+                    vehicles.Add(nou);
+                    indexVehicleActual = vehicles.Count - 1;
+                    modeActual = ModeEdicio.MODE_EDIT;
+                } 
+                else if (modeActual == ModeEdicio.MODE_EDIT)
+                {
+                    // Cerquem el vehicle actual
+                    Vehicle actual = vehicles[indexVehicleActual];
+                    // modifiquem la matrícula
+                    actual.Matricula = txbMatricula.Text;
+                    // modifiquem el tipus
+                    if (rdoCotxe.IsChecked == true)
+                    {
+                        actual.Tipus = EnumTipus.COTXE;
+                    }
+                    else
+                    {
+                        actual.Tipus = EnumTipus.MOTO;
+                    }
+                    //actual.Tipus = (rdoCotxe.IsChecked == true) ? EnumTipus.COTXE : EnumTipus.MOTO;
+                    Marca m = (Marca)cboMarca.SelectedItem;
+                    actual.Marca = m.Nom;
+
+                    actual.Model = cboModel.SelectedValue.ToString();
+
+                    //actual.Marca = cboMarca.SelectedValue.ToString();
+                }
+                // canvis desats, per tant no hi ha canvis pendents
+          
+            }
+
+
+        }
+        private bool MostraError(bool hiHaError, TextBlock t, bool estatActual)
+        {
+            bool esValid = estatActual;
+            if (hiHaError)
+            {
+                t.Visibility = Visibility.Visible;
+                esValid = false;
+            }
+            else
+            {
+                t.Visibility = Visibility.Collapsed;
+            }
+            return esValid;
+        }
+
+        private bool ValidaCamps()
+        {
+            bool esValid = true;
+
+            esValid = MostraError(cboMarca.SelectedIndex == -1, txbErrorMarca, esValid);
+            esValid = MostraError(cboModel.SelectedIndex == -1, txbErrorModel, esValid);
+            esValid = MostraError(!Vehicle.ValidaMatricula(txbMatricula.Text), txbErrorMatricula, esValid);
+
+            /*if (cboMarca.SelectedIndex==-1)
+            {
+                txbErrorMarca.Visibility = Visibility.Visible;
+                esValid = false;
+            } else
+            {
+                txbErrorMarca.Visibility = Visibility.Collapsed;
+            }
+            //----------------------------------------------------------
+            if (cboModel.SelectedIndex == -1)
+            {
+                txbErrorModel.Visibility = Visibility.Visible;
+                esValid = false;
+            }
+            else
+            {
+                txbErrorModel.Visibility = Visibility.Collapsed;
+            }
+            //----------------------------------------------------------
+
+            if(Vehicle.ValidaMatricula(txbMatricula.Text))
+            {
+                txbErrorMatricula.Visibility = Visibility.Collapsed;
+                txbMatricula.Background = new SolidColorBrush(Colors.Transparent);
+            } else
+            {
+                txbErrorMatricula.Visibility = Visibility.Visible;
+                txbMatricula.Background = new SolidColorBrush(Colors.Red);
+                esValid = false;
+            }
+            */
+            return esValid;
+        }
+
+        ModeEdicio modeActual = ModeEdicio.MODE_EDIT;
+
+        private void Button_Click_New(object sender, RoutedEventArgs e)
+        {
+            BuidarFormulari();
+            modeActual = ModeEdicio.MODE_NEW;
+        }
+
+        private void BuidarFormulari()
+        {
+            txbMatricula.Text = "";
+            cboMarca.SelectedIndex = -1;
+            cboModel.SelectedIndex = -1;
+            rdoCotxe.IsChecked = true;
+            rdoMoto.IsChecked = false;
+
+            int codiMax = -1;
+            foreach(Vehicle v in vehicles)
+            {
+                if (v.Codi > codiMax) codiMax = v.Codi;
+            }
+            codiMax++;
+            txbCodi.Text = codiMax + "";
+        }
+
+        private async void Button_Click_Esborrar(object sender, RoutedEventArgs e)
+        {
+            if (await verificaCanvisPendentsAsync())
+            {
+                //vehicles.Remove(vehicles[indexVehicleActual]);
+                vehicles.RemoveAt(indexVehicleActual);
+                if (indexVehicleActual >= vehicles.Count) indexVehicleActual--;
+                MostrarVehicle(indexVehicleActual);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MostrarVehicle(indexVehicleActual);
+
+        }
+
+        private void txbMatricula_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        
+        }
+
+        private void cboModel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+          
+        }
+
+        private void rdoCotxe_Checked(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ 4_gestio_vehicles_databinding/2_classes_i_collecions_uwp/model/Vehicle.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2_classes_i_collecions_uwp.model
+{
+    
+
+    class Vehicle
+    {
+        private static List<Vehicle> vehicles;
+
+        public static List<Vehicle> GetVehicles()
+        {
+            if (vehicles == null)
+            {
+                vehicles = new List<Vehicle>();
+                Vehicle v1 = new Vehicle(1, "1234JKJ", "Seat", "Leon", EnumTipus.COTXE);
+                Vehicle v2 = new Vehicle(20, "9999GGG", "Seat", "Exeo", EnumTipus.COTXE);
+                Vehicle v3 = new Vehicle(3, "3333JJJ", "Volkswagen", "Golf", EnumTipus.COTXE);
+                vehicles.Add(v1);
+                vehicles.Add(v2);
+                vehicles.Add(v3);
+                //vehicles.AddRange(new List<Vehicle>{ v1,v2,v3});
+            }
+            return vehicles;
+        }
+
+
+
+
+        //-----------------------------------------------
+
+
+        private int codi;
+        private string matricula;
+        private string marca;
+        private string model;
+        private EnumTipus tipus;
+
+        public Vehicle(int codi, string matricula, string marca, string model, EnumTipus tipus)
+        {
+            this.Codi = codi;
+            this.Matricula = matricula;
+            this.Marca = marca;
+            this.Model = model;
+            this.Tipus = tipus;
+        }
+
+        public int Codi { get => codi; set => codi = value; }
+        public string Matricula {
+                            get => matricula;
+                            set  {
+                                    if(ValidaMatricula(value))
+                                    {
+                                        matricula = value;
+                                    } else
+                                    {
+                                        throw new Exception("Matricula errònia");
+                                    }
+                            }
+        }
+
+
+        public static bool ValidaMatricula(String unaMatricula)
+        {
+            if (unaMatricula == null)  return false;
+     
+            // Instantiate the regular expression object.
+            Regex r = new Regex("^[0-9]{4}[ZXCVBNMSDFGHJKLQWRTYP]{3}$", RegexOptions.IgnoreCase);
+
+            // Match the regular expression pattern against a text string.
+            Match m = r.Match(unaMatricula);
+            if(m.Success) { return true; } else
+            {
+                return false;
+            }            
+        }
+
+        public override bool Equals(object obj)
+        {
+            var vehicle = obj as Vehicle;
+            return vehicle != null &&
+                   Codi == vehicle.Codi &&
+                   Matricula == vehicle.Matricula &&
+                   Marca == vehicle.Marca &&
+                   Model == vehicle.Model &&
+                   Tipus == vehicle.Tipus;
+        }
+
+        public string Marca { get => marca; set => marca = value; }
+        public string Model { get => model; set => model = value; }
+        public EnumTipus Tipus { get => tipus; set => tipus = value; }
+
+
+         
+
+
+        /*
+        public Vehicle(int pcodi, string pmatricula, string pmarca, string pmodel , string ptipus)
+        {
+            this.codi = pcodi;
+            this.matricula = pmatricula;
+        }*/
+
+
+
+
+
+    }
+}
